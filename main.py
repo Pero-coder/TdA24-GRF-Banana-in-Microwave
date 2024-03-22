a0db1748580870eb8715db641a08aadf7474c1b4
RxiPland <liborvanura@gmail.com>
2024-03-22 17:55:41 +0100

    API example test

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ client = OpenAI()
 client = OpenAI(
   api_key=os.environ.get("OPENAI_API_KEY")
 )
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "Jsi učitel, který vysvětluje python."},
+        {"role": "user", "content": "Co je knihovna flask?"}
+    ]
+)
+
+# outputs only AI response message
+print(completion.choices[0].message.content)
