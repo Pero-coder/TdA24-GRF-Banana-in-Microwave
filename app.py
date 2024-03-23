fd4844b8e9c2908d19106d4a3bda70b19b08ad64
Pero-coder <67205912+Pero-coder@users.noreply.github.com>
2024-03-23 15:18:23 +0100

    Refactor homepage template and variable names

--- app.py
+++ app.py
@@ -12,18 +12,6 @@ from bson import json_util
 import models
 import utils
 
-from flask import Flask, render_template
-from jinja2 import Environment, PackageLoader, select_autoescape
-
-app = Flask(__name__)
-
-env = Environment(
-    loader=PackageLoader('yourapplication', 'templates'),
-    autoescape=select_autoescape(['html', 'xml']),
-    extensions=['jinja2.ext.loopcontrols']
-)
-env.filters['zip'] = zip
-
 
 load_dotenv()
 
@@ -66,7 +54,9 @@ def homepage():
     found_activities: List[Dict[str, Any]] = list(activities_db.find())
     ai_generated_descriptions: List[Dict[str, Any]] = list(ai_summaries_db.find())
 
-    return render_template("homepage.html", activities=found_activities, descriptions=ai_generated_descriptions)
+    activities_and_descriptions = zip(found_activities, ai_generated_descriptions)
+
+    return render_template("homepage.html", activities=found_activities, activities_and_descriptions=activities_and_descriptions)
 
 @app.route("/aktivita")
 def activity_empty():
@@ -82,10 +72,6 @@ def aktivita_page(activity_uuid: str):
 
     return render_template("activity_page.html", raw_json=activity_get[0])
 
-
-@app.route('/tvorba-aktivity', methods=["GET"])
-def create_activity_page():
-    return render_template("create-activity_page.html")
 
 # Login page
 @app.route("/login", methods=["GET", "POST"])
@@ -198,43 +184,8 @@ def delete_activity(activity_uuid: str):
     else:
         return {"code": 200, "message": "Activity deleted successfully"}, 200
 
-@app.route("/api/search_ai", methods=["POST"])
-def search_activities_ai():
-
-    request_json: dict = request.get_json() # {"prompt": "", "quantity": ""}
-
-    search_text: str|None = request_json.get("prompt")
-    quantity: str|None = request_json.get("quantity")
-
-    if search_text is None:
-        return {"code": 400, "message": "Prompt text is not in json"}, 400
-    
-    search_text = search_text.strip()
-    if search_text == "":
-        return {"code": 400, "message": "Prompt cannot be empty"}, 400
 
 
-    return {"uuids": utils.ai_search_activities(search_text, 15)}, 200
-
-
-# admin api
-@app.route("/api/approve_activity", methods=["POST"])
-def approve_activity_admin():
-    request_json: dict = request.get_json() # {"uuid": ""}
-
-    activity_uuid: str|None = request_json.get("uuid")
-
-    if activity_uuid is None:
-        return {"code": 400, "message": "Not valid uuid"}, 400
-    
-    success = utils.approve_activity(activity_uuid)
-
-    if success:
-        return {"code": 200, "message": "Activity approved"}, 200
-    else:
-        return {"code": 400, "message": "Failed to aprove activity"}, 400
-    
-     
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
