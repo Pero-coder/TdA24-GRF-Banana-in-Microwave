81169fdd5df12c7af8d3c66623a0a332e35905a5
RxiPland <liborvanura@gmail.com>
2024-03-23 10:18:56 +0100

    Create new activity

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 from pymongo.mongo_client import MongoClient
 from datetime import timedelta
 import secrets
 import os
+from typing import List, Dict, Any
+import json
+from bson import json_util
+
+import models
+import utils
+
 
 load_dotenv()
 
@@ -14,7 +21,11 @@ mongodb_client = MongoClient(
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)  # Generate a random secret key
 app.permanent_session_lifetime = timedelta(hours=4)  # Session expires after 4 hours
+
 db = mongodb_client.project_activities_database
+activities_db = db.activities
+credentials_db = db.credentials
+
 
 openai_client = OpenAI(
   api_key=os.environ.get("OPENAI_API_KEY")
@@ -32,6 +43,29 @@ completion = openai_client.chat.completions.create(
 @app.route('/')
 def hello_world():
     return completion.choices[0].message.content
+
+
+# APIs
+
+@app.route('/api/activity', methods=["GET", "POST"])
+def create_activity():
+
+    if request.method == 'POST':
+        request_json = request.get_json()
+
+        new_activity_object = models.NewActivity(**request_json)
+        utils.add_activity_to_db(new_activity_object)
+    
+    elif request.method == 'GET':
+        pass
+
+
+    found_activities: List[Dict[str, Any]] = list(activities_db.find())
+
+    # TODO: change to specific activity (not all)
+    return json.loads(json_util.dumps(found_activities)), 200
+
+
 
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List, Union
+
+class Image(BaseModel):
+    lowRes: str
+    highRes: str
+
+class Gallery(BaseModel):
+    title: str
+    images: List[Image]
+
+class Link(BaseModel):
+    title: str
+    url: str
+
+class Agenda(BaseModel):
+    duration: int
+    title: str
+    description: str
+
+class Instruction(BaseModel):
+    title: str
+    warn: str
+    note: str
+
+class HomePreparation(BaseModel):
+    title: str
+    warn: str
+    note: str
+
+class NewActivity(BaseModel):
+    uuid: str
+    activityName: str
+    description: str
+    objectives: List[str]
+    classStructure: str
+    lengthMin: int
+    lengthMax: int
+    edLevel: Optional[List[str]]
+    tools: Optional[List[str]]
+    homePreparation: Optional[List[HomePreparation]]
+    instructions: Optional[List[Instruction]]
+    agenda: Optional[List[Agenda]]
+    links: Optional[List[Link]]
+    gallery: Optional[List[Gallery]]
+
+    class Config:
+        extra = "ignore"
