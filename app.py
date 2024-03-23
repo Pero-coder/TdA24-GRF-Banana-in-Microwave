f8e186bc6df4b64a22e28e2b3db413018c31a744
RxiPland <liborvanura@gmail.com>
2024-03-23 09:24:27 +0100

    Connecting to mongo db

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 from flask import Flask
+from pymongo.mongo_client import MongoClient
+from datetime import timedelta
+import secrets
 import os
 
 load_dotenv()
 
+mongodb_client = MongoClient(
+    f'mongodb+srv://{os.environ.get("MONGO_USERNAME")}:{os.environ.get("MONGO_PWD")}@cluster0.ebiunpa.mongodb.net/?retryWrites=true&w=majority'
+)
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(32)  # Generate a random secret key
+app.permanent_session_lifetime = timedelta(hours=4)  # Session expires after 4 hours
+db = mongodb_client.project_activities_database
 
-client = OpenAI(
+openai_client = OpenAI(
   api_key=os.environ.get("OPENAI_API_KEY")
 )
 
 # request to chatgpt API
-completion = client.chat.completions.create(
+completion = openai_client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "Učitel Masarykovy univerzity v Brně"},
--- dockerfile
+++ dockerfile
@@ -2,6 +2,10 @@ FROM python:3.10-buster
 WORKDIR /
 ARG OPENAI_API_KEY
 ENV OPENAI_API_KEY=$OPENAI_API_KEY
+ARG MONGO_PWD
+ENV MONGO_PWD=$MONGO_PWD
+ARG MONGO_USERNAME
+ENV MONGO_USERNAME=$MONGO_USERNAME
 COPY . .
 RUN pip install -r requirements.txt
 ENTRYPOINT python3 -m flask --app app.py run --host=0.0.0.0 --port=80
